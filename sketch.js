0eb1f6e78f5ad121e6f2c95a449c78cc0e124282
Tatiana Moreno <telizabeth.morenoc@ucuenca.edu.ec>
2024-09-30 16:27:21 -0500

    Movimiento de flechas
    Se agregaron variables para que el circulo se mueva segun las flechas del teclado

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
 let circleColor;
+let circleX, circleY;
+let speed = 80; // Velocidad del círculo
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   circleColor = color(255); // Color inicial blanco
+  circleX = width / 2; // Posición inicial en el centro
+  circleY = height / 2;
 }
 
 function draw() {
   background(0); // Para que se borre el fondo en cada frame
   fill(circleColor);
   noStroke();
-  ellipse(width / 2, height / 2, 200, 200); // Dibuja el círculo
+  ellipse(circleX, circleY, 200, 200); // Círculo
 }
 
 // Función para detectar teclas presionadas
@@ -24,5 +28,18 @@ function keyTyped() {
     circleColor = color(0, 0, 255); // Azul
   } else if (key === "e" || key === "E") {
     circleColor = color(0); // Borra el círculo
+  }
+}
+
+// Función para mover el círculo con las flechas del teclado
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    circleX -= speed; // Mueve a la izquierda
+  } else if (keyCode === RIGHT_ARROW) {
+    circleX += speed; // Mueve a la derecha
+  } else if (keyCode === UP_ARROW) {
+    circleY -= speed; // Mueve hacia arriba
+  } else if (keyCode === DOWN_ARROW) {
+    circleY += speed; // Mueve hacia abajo
   }
 }
