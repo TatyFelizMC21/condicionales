ad5175a39b44f7d3bd390f9a13f29190fa14cc60
Tatiana Moreno <telizabeth.morenoc@ucuenca.edu.ec>
2024-09-30 16:15:42 -0500

    Funciona la parte 1

--- sketch.js
+++ sketch.js
@@ -3,23 +3,26 @@ let circleColor;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  circleColor = color(255);
+  circleColor = color(255); // Color inicial blanco
 }
 
 function draw() {
+  background(0); // Para que se borre el fondo en cada frame
   fill(circleColor);
   noStroke();
-  ellipse(width / 2, height / 2, 200, 200);
+  ellipse(width / 2, height / 2, 200, 200); // Dibuja el círculo
+}
 
-  // Función para detectar teclas presionadas
-  function keyPressed() {
-    // Se cambia el color según la tecla presionada
-    if (key === "r" || key === "R") {
-      circleColor = color(255, 0, 0); // Rojo
-    } else if (key === "g" || key === "G") {
-      circleColor = color(0, 255, 0); // Verde
-    } else if (key === "b" || key === "B") {
-      circleColor = color(0, 0, 255); // Azul
-    }
+// Función para detectar teclas presionadas
+function keyTyped() {
+  // Cambia el color según la tecla presionada
+  if (key === "r" || key === "R") {
+    circleColor = color(255, 0, 0); // Rojo
+  } else if (key === "g" || key === "G") {
+    circleColor = color(0, 255, 0); // Verde
+  } else if (key === "b" || key === "B") {
+    circleColor = color(0, 0, 255); // Azul
+  } else if (key === "e" || key === "E") {
+    circleColor = color(0); // Borra el círculo
   }
 }
